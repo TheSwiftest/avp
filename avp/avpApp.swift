b06cf3312b50616905aa376af6666094e94a5547
Brian <brian.william.corbin@gmail.com>
2022-06-03 12:53:01 -0700

    initial commit

---
+++ avp/avpApp.swift
@@ -0,0 +1,17 @@
+//
+//  avpApp.swift
+//  avp
+//
+//  Created by Brian Corbin on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct avpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
